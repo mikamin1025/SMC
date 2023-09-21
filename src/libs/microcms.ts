7c8182d81eb0506e30a7d014cc8a041dafec4c25
mikamin1025 <124058782+mikamin1025@users.noreply.github.com>
2023-09-22 00:34:31 +0900

    Add files via upload

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,11 +1,11 @@
 //SDK利用準備
-import { createClient } from "microcms-js-sdk";
+import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
 const client = createClient({
   serviceDomain: import.meta.env.SERVICE_DOMAIN,
   apiKey: import.meta.env.API_KEY,
 });
 
 //APIの呼び出し
-export const getEventDate = async () => {
-  return await client.get({ endpoint: "event_date" });
+export const getEventDate = async (queries?: MicroCMSQueries) => {
+  return await client.get({ endpoint: "event_date", queries });
 };
